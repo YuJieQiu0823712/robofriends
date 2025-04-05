595ce43244e80fff6a35174c5c9c6b6c1333fc51
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-04-05 22:55:15 +0200

    use componentDidMount to update state

--- robofriends/src/App.jsx
+++ robofriends/src/App.jsx
@@ -10,10 +10,19 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
+
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }))
+        
+    }
+
 
     // onSearchChange(event) {
         // console.log(event.target.value) // target => element that triggered the event
@@ -28,15 +37,21 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         }) 
-        return (
-            <div className='tc'>
-                <h1 className='f1'>Robofriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        )
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            )
+        }
     }
 
 }
 
 export default App
+
+
