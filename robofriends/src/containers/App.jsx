5fde1c2282ec55c8e2a554e24640b434fbb6bb6d
YuJieQiu0823712 <chiou.jade@gmail.com>
2025-04-07 12:50:24 +0200

    use React Hooks

--- robofriends/src/containers/App.jsx
+++ robofriends/src/containers/App.jsx
@@ -1,56 +1,62 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import CardList from '../components/CardList'
-// import { robots } from './robots'
 import SearchBox from '../components/SearchBox'
 import './App.css'
 import Scroll from './Scroll'
 
 
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+function App () {
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: ''
+    //     }
+    // }
+
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
+    const [count, setCount] = useState(0)
 
 
-    componentDidMount(){
+    // componentDidMount(){
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(response => response.json())
+    //     .then(users => this.setState({ robots: users }))
+    // }
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(users => this.setState({ robots: users }))
-        
-    }
+        .then(users => setRobots(users))
+        console.log(count)
+    }, [count]) 
+    // [] means it will only run once when the component mounts, [count] means it will run when count changes
 
 
-    // onSearchChange(event) {
-        // console.log(event.target.value) // target => element that triggered the event
-        // const filteredRobots = this.state.robots.filter(robot => { // this refers SearchBox.jsx input, input does not have state => use arrow function 
-    onSearchChange = (event) => { 
-        this.setState({ searchfield: event.target.value})   
+    
+    const onSearchChange = (event) => { 
+        setSearchfield(event.target.value)   
         
     }
-    
-    
-    render() {
-        const { searchfield, robots } = this.state // destructuring}
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        }) 
-        return !robots.length ? // reobots.length === 0
-             <h1>Loading</h1> :
-            (
-                <div className='tc'>
-                    <h1 className='f1'>Robofriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <CardList robots={filteredRobots}/>
-                    </Scroll>
-                </div>
-            )
-    }
+    // const { searchfield, robots } = this.state // destructuring
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    }) 
+    return !robots.length ? // reobots.length === 0
+            <h1>Loading</h1> :
+        (
+            <div className='tc'>
+                <h1 className='f1'>Robofriends</h1>
+                <button onClick={() => setCount(count+1)}>Click Me</button>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        ) //onclick => arrow function that will be executed when the button is clicked.
+
 }
 
 export default App
